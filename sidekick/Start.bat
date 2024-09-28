f83b873de4ef880a43ab0ced4b3deb6672477dc4
femi122 <36718436+femi122@users.noreply.github.com>
2024-09-28 10:58:45 +0100

    this is for sidekick

---
+++ sidekick/Start.bat
@@ -0,0 +1,34 @@
+@echo off
+title Sidekick - 69trading.com
+setlocal
+
+:: Open terminal here
+cd /d %~dp0
+
+:: Set Welcome
+echo Tool Free - 69trading
+
+:: Check version Node.js
+node -v >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Node.js Not installed yet. Please install Node.js first
+    exit /b 1
+)
+
+:: Run npm install
+if not exist node_modules (
+    echo The node_modules directory not found. Run npm install...
+    npm install
+    npm install ws
+) else (
+    echo Installed successfully. 69trading.com
+)
+
+:: Run index.js for Node.js
+echo Loading Tool...
+node kick.js
+
+:: pause
+pause
+
+endlocal
